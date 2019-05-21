ef33ed1a0c5189a5389c035f1904e46a98a86bc5
mightyjol <jerome.hanke@gmail.com>
2019-05-21 08:20:27 +0200

    test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -57,5 +57,5 @@
 	<figcaption>HIGH FIVE!</figcaption>
 </figure>
 
-<p><strong>UPDATED ---- v0.0.9</strong></p>
+<p><strong>UPDATED ---- v0.0.10</strong></p>
 <button on:click="{checkForUpdate}">check for update</button>
